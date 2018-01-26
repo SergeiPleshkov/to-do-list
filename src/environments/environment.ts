11dd55a53692c5a0d5d017e533f04d901d3fe04a
Sergei Pleshkov <azrailomgpro@gmail.com>
2018-01-26 17:59:46 +0300

    Project created. Added bootstrap, firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAepe_DgDXxrwMtiyhwpYxsKg34fx46Ufg",
+    authDomain: "to-do-list-f8911.firebaseapp.com",
+    databaseURL: "https://to-do-list-f8911.firebaseio.com",
+    projectId: "to-do-list-f8911",
+    storageBucket: "to-do-list-f8911.appspot.com",
+    messagingSenderId: "258352773380"
+  }
 };
